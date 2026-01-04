b47be2de2724108d8fef7800404a0cd0b5e2a5a0
Fahim <conkarer@gmail.com>
2026-01-04 18:47:38 +0500

    Update project.py

--- project.py
+++ project.py
@@ -1,15 +1,13 @@
-# -------------------------------
+
 # Mini Statistical Analyzer
-# -------------------------------
 
 # Step 1: Take dataset input from user
 data_input = input("Enter numbers separated by space: ")
 data = data_input.split()  # Split input into list of strings
 data = [int(x) for x in data]  # Convert strings to integers
 
-# -------------------------------
 # Frequency Distribution
-# -------------------------------
+
 freq = {}
 for num in data:
     if num in freq:
@@ -21,15 +19,15 @@ print("\nFrequency Distribution:")
 for key in freq:
     print(f"{key}: {freq[key]}")
 
-# -------------------------------
+
 # Mean
-# -------------------------------
+
 mean = sum(data) / len(data)
 print("\nMean:", mean)
 
-# -------------------------------
+
 # Median
-# -------------------------------
+
 data_sorted = sorted(data)
 n = len(data_sorted)
 middle = n // 2
@@ -71,6 +69,7 @@ print("Standard Deviation:", std_dev)
 print("\nHistogram:")
 for key in sorted(freq):
     print(f"{key}:", "*" * freq[key])
+
 
 
 
